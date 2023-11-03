5c8e5a9eb0bc9c8752d559e27d830c9562836759
Harry Chong <harryhuayuanchong@gmail.com>
2023-11-02 19:26:57 -0700

    testing: test my gpgkey

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,6 +7,7 @@ export default function Welcome() {
         <div className="wrapper">
             <h1>Welcome To My App</h1>
             <p>This is going to be the coolest app in the world!</p>
+            <p>Testing</p>
             <img src={Sammy} alt="Sammy Image" width={200} height={200} />
         </div>
     </>
